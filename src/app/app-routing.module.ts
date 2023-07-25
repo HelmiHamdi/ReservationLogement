6408785991367ce885bd537d6297bf865edb20bb
HelmiHamdi <helmihamdi977@gmail.com>
2023-07-25 09:21:03 +0100

    hgf

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
+import { HoteComponent } from './components/hote/hote.component';
 
 const routes: Routes = [
   {path:'',component:HeaderComponent},
-  {path:'home',component:HomeComponent}
+  {path:'home',component:HomeComponent},
+  {path:'hote',component:HoteComponent}
 ];
 
 @NgModule({
---
+++ src/app/components/hote/hote.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hote',
+  templateUrl: './hote.component.html',
+  styleUrls: ['./hote.component.css']
+})
+export class HoteComponent {
+
+}
